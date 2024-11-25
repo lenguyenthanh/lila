9e747d62196ea55250d42db4669b063dc7280f50
Thanh Le <lenguyenthanh@hotmail.com>
2024-11-25 20:01:18 +0100

    Clean up unsued values

--- modules/studySearch/src/main/package.scala
+++ modules/studySearch/src/main/package.scala
@@ -1,8 +1,3 @@
 package lila.studySearch
 
 export lila.core.lilaism.Lilaism.{ *, given }
-export lila.common.extensions.*
-
-private val logger = lila.log("studySearch")
-
-private val index = lila.search.Index.Study
